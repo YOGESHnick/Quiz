678194d58017e0e11d1a5367c9dc9d6acf078f37
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-08-07 23:35:25 +0530

    new screen rendered

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,21 @@ import 'package:quiz/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors:  [
+                Color.fromARGB(255, 0, 0, 0),
+                Color.fromARGB(255, 160, 89, 226),
+              ],
+              begin: Alignment.bottomLeft,
+              end:Alignment.topRight,
+            ),
+            ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-
   const StartScreen({super.key});
 
   @override
-  Widget build(context){
-    return const Text("Start Screen");
+  Widget build(context) {
+    return Center(
+      child: Column(mainAxisSize: MainAxisSize.min, children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          width: 300,
+        ),
+        const SizedBox(height: 90),
+        const Text(
+          'Learn Flutter the fun way!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+        ),
+        const SizedBox(height: 60,),
+        OutlinedButton(onPressed: (){}, 
+        style: OutlinedButton.styleFrom(
+          foregroundColor: Color.fromRGBO(255, 255, 255, 1),
+        ),
+        child: const Text("--> Get started")),
+      ]),
+    );
   }
 }
