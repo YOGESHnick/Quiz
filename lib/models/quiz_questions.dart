8fb136b18f10a4c55dc8d6907d425d26ebd82083
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-08-14 12:30:15 +0530

    added shuffling functions to shuffle answers everytime

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -2,4 +2,10 @@ class QuizQuestion {
   const QuizQuestion(this.text,this.answers);
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers(){
+    final shuffledList = List.of(answers);     // List.of(answers) creates a list copy of answers
+    shuffledList.shuffle();// shuffles the list inplace, thats y we're copying original list here 
+    return shuffledList;
+  }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -20,20 +20,25 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     return SizedBox(
       width: double.infinity,
       child: 
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-          Text(currentQuestion.text,
-          style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers.map((answer){     //here map converts string to AnswerButton , 
-          return AnswerButton(      //... adds the AnswerButton as individual widgets
-            answerText: answer, onTap: (){}
-            );
-          }),
-        ],
-      ),
+        Container(
+          margin:const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+            Text(currentQuestion.text,
+            style: const TextStyle(color: Colors.white,fontSize: 17),
+            textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer){     //here map converts string to AnswerButton , 
+            return AnswerButton(      //... adds the AnswerButton as individual widgets
+              answerText: answer, onTap: (){}
+              );
+            }),
+          ],
+              ),
+        ),
     );
   }
 }
